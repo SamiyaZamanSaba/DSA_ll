67ced538b3e5b4711d4a336586f4e0a8a44fd96e
Saba <samiyazamansaba1@gmail.com>
2026-03-31 17:31:40 +0600

    Add files via upload

---
+++ Ferris_Wheel.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    long long x;
+    cin >> n >> x;
+
+    vector<long long> p(n);
+    for (int i = 0; i < n; i++) cin >> p[i];
+
+    sort(p.begin(), p.end());
+
+    int i = 0, j = n - 1;
+    int gondolas = 0;
+
+    while (i <= j) {
+        if (p[i] + p[j] <= x) {
+            i++;  // pair them
+        }
+        j--;      // heaviest always goes
+        gondolas++;
+    }
+
+    cout << gondolas << endl;
+    return 0;
+}
