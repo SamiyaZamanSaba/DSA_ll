4970b7bb5614e4e176dd81c918288a6ef348427d
Saba <samiyazamansaba1@gmail.com>
2026-03-31 17:29:51 +0600

    Add files via upload

---
+++ Concert_Tickets.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    multiset<int> tickets;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        tickets.insert(x);
+    }
+
+    while (m--) {
+        int t;
+        cin >> t;
+
+        auto it = tickets.upper_bound(t);
+
+        if (it == tickets.begin()) {
+            cout << -1 << endl;
+        } else {
+            it--;
+            cout << *it << endl;
+            tickets.erase(it);
+        }
+    }
+
+    return 0;
+}
