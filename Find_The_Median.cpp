084ee1922174974b5398a189b830ef196c4b036f
Saba <samiyazamansaba1@gmail.com>
2026-04-13 14:02:57 +0600

    Add files via upload

---
+++ Find_The_Median.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findMedian(vector<int> arr) {
+    sort(arr.begin(), arr.end());
+    return arr[arr.size() / 2];
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << findMedian(arr) << endl;
+
+    return 0;
+}
